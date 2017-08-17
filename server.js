f6782f0a65498584516c3d227296d59e11ac602c
Joseph Joyce <joej326@gmail.com>
2017-08-16 19:52:05 -0700

    jj

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+
 const massive = require('massive');
 
 const path = require('path');
@@ -8,6 +9,24 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 // app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.use(bodyParser.json());
+
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+
+
+
+//*************************************************
+
+
+
+
 
 
 app.listen(8017, function(){
